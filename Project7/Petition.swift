4c1d58bdb297f10b95c3c533435d5e1001c1844d
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-09-15 11:33:17 -0500

    Add Project7

---
+++ Project7/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Victor Hugo on 14/09/19.
+//  Copyright © 2019 Vintage Robot. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable
+{
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Victor Hugo on 14/09/19.
+//  Copyright © 2019 Vintage Robot. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
