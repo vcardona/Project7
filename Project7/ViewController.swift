dfb6cfdd558916d2011d401e5aab955b4bcc01a4
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-09-15 23:22:28 -0500

    Add DispatchQueue qos userInitiated

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -21,12 +21,16 @@ class ViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
 
-        if let url = URL(string: urlString){
-            if let data = try? Data(contentsOf: url){
-                parse(json: data)
-                return
+        DispatchQueue.global(qos: .userInitiated).async {
+            [weak self] in
+            if let url = URL(string: urlString){
+                if let data = try? Data(contentsOf: url){
+                    self?.parse(json: data)
+                    return
+                }
             }
         }
+        
         showError()
     }
     
